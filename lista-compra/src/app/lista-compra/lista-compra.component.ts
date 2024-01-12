bf033bd6cffb6ddcff5043e265f6ba78985c74e9
aas141-ua <aas141@alu.ua.es>
2024-01-12 11:00:54 +0100

    Añadido botón eliminar y añadir
    Lo único que falta es que cuando se añada un prodcuto, se borre lo que se haya escrito en el input.

--- lista-compra/src/app/lista-compra/lista-compra.component.ts
+++ lista-compra/src/app/lista-compra/lista-compra.component.ts
@@ -12,14 +12,21 @@ import { ClientApiService } from '../client-api.service';
   <ul>
     <li *ngFor="let item of items" (click)="toggleCompra(item)">
       <span [class.tachado]="item.comprado">{{ item.nombre }}</span>
+      <button (click)="eliminarItem(item.id)">Eliminar</button>
     </li>
   </ul>
+  <div>
+      <input type="text" id="nuevoItemNombre" placeholder="Nuevo item" (input)="onInputChange($event)" />
+      <button (click)="agregarItem()">Agregar</button>
+    </div>
+
 
   `,
   styleUrl: './lista-compra.component.css'
 })
 export class ListaCompraComponent {
   items: Item[] = []
+  nuevoItemNombre = '';
   api: ClientApiService = inject(ClientApiService);
 
   constructor() {
@@ -29,5 +36,39 @@ export class ListaCompraComponent {
   }
   toggleCompra(item: Item): void {
     item.comprado = !item.comprado;
+  }
+  async eliminarItem(itemId: number): Promise<void> {
+    const confirmacion = confirm(`¿Seguro que deseas eliminar el producto?`);
+
+    if (confirmacion) {
+      const exito = await this.api.delItem(itemId);
+
+      if (exito) {
+        // Actualiza la lista después de eliminar el producto
+        this.items = this.items.filter(item => item.id !== itemId);
+      } else {
+        // Maneja el caso en que la eliminación no sea exitosa
+        console.error('Error al eliminar el producto.');
+      }
+    }
+  }
+  onInputChange(event: any): void {
+    // Manejar el cambio en el input
+    this.nuevoItemNombre = event.target.value;
+  }
+
+  async agregarItem(): Promise<void> {
+    if (this.nuevoItemNombre.trim() !== '') {
+      const nuevoItem = await this.api.addItem(this.nuevoItemNombre);
+
+      // Agregar el nuevo ítem a la lista
+      this.items.push(nuevoItem);
+
+      // Limpiar el campo de entrada después de agregar el ítem
+      this.nuevoItemNombre = '';
+    } else {
+      // Manejar caso en que el nombre del ítem es vacío
+      console.error('Nombre del nuevo ítem no puede estar vacío.');
+    }
   }
 }
